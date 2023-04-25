bfd6a1d3241a5d28f362256c3b2f8998ba336e3a
Aaron Klump <sourcecode@intheloftstudios.com>
2023-04-25 15:29:24 -0700

    Improve the output formatting.  Speed up execution.  Add support for reading info.yml files, composer.json and package.json files.

--- version_compare.sh
+++ version_compare.sh
@@ -18,35 +18,79 @@ function on_pre_config() {
   [[ "$(get_command)" == "init" ]] && exit_with_init
 }
 
-function echo_version() {
-  version=$("$HOME/bin/web_package" v | tail -n1)
-  if [[ ! "$version" ]]; then
-    echo "(not found)"
+# Echo the composer package name (if available)
+#
+# Returns 1 if not found.
+function echo_composer_name() {
+  if [ -f composer.json ]; then
+    json_set "$(cat composer.json)"
+    name="$(json_get_value "name")"
+    [[ "$name" ]] && echo "$name" && return 0
   fi
-  echo "$version"
+  return 1
 }
 
-function compare() {
-  local module="$1"
+function echo_version() {
 
-  # We can't compare a nameless module.
-  if ! [[ "$module" ]]; then
-    return 1
+  if [ -f *.info.yml ]; then
+    yaml_set "$(cat *.info.yml)"
+    json_set "$(yaml_get_json)"
+    version="$(json_get_value "version")"
+    [[ "$version" ]] && echo "$version" && return 0
+  fi
+  # TODO Read from .info file, i.e., D7.
+
+  if [ -f composer.json ]; then
+    json_set "$(cat composer.json)"
+    version="$(json_get_value "version")"
+    [[ "$version" ]] && echo "$version" && return 0
   fi
 
-  # If not in the library we can't compare.
-  if [ ! -e "$library_dir/$module" ]; then
+  if [ -f package.json ]; then
+    json_set "$(cat package.json)"
+    version="$(json_get_value "version")"
+    [[ "$version" ]] && echo "$version" && return 0
+  fi
+
+  version=$("$HOME/bin/web_package" v | tail -n1)
+  [[ "$version" ]] &&  echo "$version" && return 0
+
+  echo "?"
+  return 1
+}
+
+# Echo a JSON string of version info.
+#
+# $1 - string The module name
+#
+# Returns 0 if .
+function get_project_version_json() {
+  local module="$1"
+
+  # We can't compare a nameless or missing module.
+  if ! [[ "$module" ]] || [ ! -e "$project_dir/$module" ]; then
+    echo '{}'
     return 1
   fi
 
   local installed_version=$(cd "$project_dir/$module" && echo_version)
+  local composer=$(cd "$project_dir/$module" && echo_composer_name)
+
+  # If not in the library we can't compare.
+  if [ ! -e "$library_dir/$module" ]; then
+    echo "{\"name\":\"$module\",\"composer\":\"$composer\",\"path\":\"$project_dir/$module\",\"status\":\"1\",\"version\":\"$installed_version\",\"available\":\"\"}"
+    return 0
+  fi
+
   local lib_version=$(cd "$library_dir/$module" && echo_version)
 
+  local status=0
   if [[ "$installed_version" == "$lib_version" ]]; then
-    echo "$module: $installed_version -> $lib_version"
-  else
-    echo_red_highlight "$module: $installed_version -> $lib_version"
+    status=1
   fi
+
+  echo "{\"name\":\"$module\",\"composer\":\"$composer\",\"path\":\"$project_dir/$module\",\"status\":\"$status\",\"version\":\"$installed_version\",\"available\":\"$lib_version\"}"
+  return 0
 }
 
 # Begin Cloudy Bootstrap
@@ -74,16 +118,41 @@ case $command in
   eval $(get_config_path_as "project_dir" "installed_modules" )
   eval $(get_config_path_as "library_dir" "source_modules" )
 
-  echo_title "Checking custom modules versions in:  $project_dir..."
-  echo_heading "Project: Installed version -> Available Version"
+  echo_title "Reviewing Custom Modules"
+  echo_heading $(path_relative_to_pwd "$project_dir")
   list_clear
-  for module in "$project_dir"/*; do
-    if [ -d "$module" ]; then
-      project_name=${module##*/}
-      list_add_item "$(compare "$project_name")"
+
+  todos=()
+  directories=("$project_dir"/*)
+  i=1
+  for directory in ${directories[@]}; do
+    if [ -d "$directory" ]; then
+      project_name=${directory##*/}
+      json_set "$(get_project_version_json "$project_name")"
+
+      id=$(json_get_value "composer")
+      [[ ! "$id" ]] && id=$(json_get_value "name")
+
+      bullet=$LI
+      [ $i -eq ${#directories[@]} ] && bullet=$LIL
+
+      if [[ $(json_get_value "status") != '1' ]]; then
+        available="$(json_get_value "available")"
+        todos=("${todos[@]}" "$id:$available")
+        echo $bullet "$(echo_red_highlight "$id:$(json_get_value "version") -> $available")"
+      else
+        echo "$bullet $id:$(json_get_value "version")"
+      fi
     fi
+    ((i++))
   done
-  echo_list
+
+  echo
+  echo_heading "todos.md"
+  for item in "${todos[@]}"; do
+    echo "- Update to $item"
+  done
+
   has_failed && exit_with_failure
   exit_with_success "Version check complete."
   ;;
